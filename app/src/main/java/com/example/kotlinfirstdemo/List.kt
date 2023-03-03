2fe80f3ab9a6bc750ced9926b00f4e4630be110f
Atsushi Nakatsugawa <nakatsugawa@AtsushinoMac-mini.local>
2023-03-03 18:53:56 +0900

    Remove NCMB

--- app/src/main/java/com/example/kotlinfirstdemo/List.kt
+++ app/src/main/java/com/example/kotlinfirstdemo/List.kt
@@ -4,29 +4,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
-/*
-import com.nifcloud.mbaas.core.NCMBCallback
-import com.nifcloud.mbaas.core.NCMBObject
-import com.nifcloud.mbaas.core.NCMBQuery
-
-*/
 
 @Composable
 fun ListScreen() {
     // ↓ エラーにならないためのダミー
     var ary = remember { mutableStateOf<List<Any>>(emptyList()) }
     // リストを読み込む部分(後述）
-    /*
-    var ary = remember { mutableStateOf<List<NCMBObject>>(emptyList()) }
-    // Memoクラスを検索するクエリー
-    val query = NCMBQuery.forObject("Memo")
-    query.findInBackground(NCMBCallback { e, results ->
-        if (e == null) {
-            // 結果をaryに適用
-            ary.value = results as List<NCMBObject>
-        }
-    })
-    */
     LazyColumn(
     ) {
         items(ary.value) { obj ->
--- app/src/main/java/com/example/kotlinfirstdemo/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfirstdemo/MainActivity.kt
@@ -8,19 +8,11 @@ import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinfirstdemo.ui.theme.KotlinFirstDemoTheme
-// import com.nifcloud.mbaas.core.NCMB
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // NCMBの初期化
-        /*
-        NCMB.initialize(
-            this.getApplicationContext(),
-            "af464eb21c4723c258b3d0dec841ce960ae313e846e9b081cdbdb2a2d38d4ccf",
-            "18e6b393060588dbfb88363396078fea0843a3324bc9b69cbc86d7d34ec2e9fd"
-        )
-        */
         setContent {
             MemoBottomNavigation()
         }
